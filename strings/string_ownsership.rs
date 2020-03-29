aca6a89a5dedebcfda118960615d8f41d6828dcd
sum12 <sumitjami@gmail.com>
2020-03-29 20:31:53 +0200

    return string tuple from string instead of passing reference to string

--- strings/string_ownsership.rs
+++ strings/string_ownsership.rs
@@ -3,9 +3,17 @@ fn main() {
 
     println!("{}", calc_len(&s));
 
+    let (x, s) = ret_tuple(s);
+
+    println!("length from tuple {}", x);
+
     println!("{}", s);
 }
 
 fn calc_len(s: &String) -> usize {
     s.len()
+}
+
+fn ret_tuple(x: String) -> (usize, String) {
+    (x.len(), x)
 }
