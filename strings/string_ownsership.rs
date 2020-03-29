276ffee994c7e5021d0882f89094cf30d30c7b5d
sum12 <sumitjami@gmail.com>
2020-03-29 19:55:50 +0200

    pass string by reference

--- strings/string_ownsership.rs
+++ strings/string_ownsership.rs
@@ -1,9 +1,11 @@
 fn main() {
     let s = String::from("get length of this string");
 
-    println!("{}", calc_len(s));
+    println!("{}", calc_len(&s));
+
+    println!("{}", s);
 }
 
-fn calc_len(s: String) -> usize {
+fn calc_len(s: &String) -> usize {
     s.len()
 }
