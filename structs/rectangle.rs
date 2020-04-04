954d3b360f628fddef973dfe81c8f2aea559d8a6
sum12 <sumitjami@gmail.com>
2020-04-04 22:15:56 +0200

    area of rectangle

--- structs/rectangle.rs
+++ structs/rectangle.rs
@@ -3,11 +3,16 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+
+fn area(r: &Rectangle) -> u32 {
+    r.width * r.height
+}
 fn main() {
     let r = Rectangle {
         width: 2,
         height: 10,
     };
 
+    println!("The area is : {}", area(&r));
     println!("The rectangle is {:?}", r);
 }
